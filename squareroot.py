ab9400e8aca9f4d2b896f0016924a9a85da44d11
Cian Gallagher <ciangall1@gmail.com>
2024-02-28 12:31:45 +0000

    init commit- Function defined

---
+++ squareroot.py
@@ -0,0 +1,6 @@
+# Weekly task 06
+# Author Cian Gallagher
+# A function that approximates the square root of a positive floating point number input.
+# A tolerance of 1 decimal place is used per task spec example.
+
+def sqrt(n):
