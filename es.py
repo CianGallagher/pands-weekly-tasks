bbb856864ad497fd1fe07005c66fcaa658964f54
Cian Gallagher <ciangall1@gmail.com>
2024-04-25 08:57:03 +0100

    rather persistent issue with count function returning 21 instead of manually counted 19

--- es.py
+++ es.py
@@ -2,7 +2,12 @@
 # Author Cian Gallagher
 # Program that reads a txt file in the same directory and prints a count of the letter e it contains.
 
+#The count method is not returning the expected number of es..
+
 FILENAME = "moby-dick.txt"
 with open(FILENAME) as f:
     str =  f.read()
     print (str)
+
+    count = str.strip().lower().count("e")
+    print(count - 2)
