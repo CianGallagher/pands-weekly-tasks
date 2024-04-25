dad7a4fdc6f2e259d7c729c558ed3703f086942c
Cian Gallagher <ciangall1@gmail.com>
2024-04-25 18:52:28 +0100

    histogram and function now display on the same set of axes

--- plottask.py
+++ plottask.py
@@ -1,6 +1,6 @@
 # Weekly task 08
 # Author Cian Gallagher
-# matlotlib used to create 1. A random standard distribution and d 2. plot the function h(x)=x^3 in the range 0 to 10. 
+# matplotlib used to create 1. A random standard distribution and d 2. Plot the function h(x)=x^3 in the range 0 to 10. 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,10 +9,16 @@ import matplotlib.pyplot as plt
 normData = np.random.normal(5, 2, 1000)
 
 # Plotting the normData histogram
-plt.hist(normData, bins = 50, density = True, color = 'whitesmoke', edgecolor = 'midnightblue')
+plt.hist(normData, bins = 50, color = 'whitesmoke', edgecolor = 'midnightblue', label='Normal Distribution')
+
+# Plotting h(x) = x^3 function
+xpoints = np.linspace(0, 10, 10)
+ypoints = xpoints ** 3 # y equals x cubed
+plt.plot(xpoints, ypoints, color = 'green', label = 'h(x) = x^3' )
 
 plt.xlabel('Values')
 plt.ylabel('Frequency')
-plt.title('Histogram of Normal Distribution (mean = 5, stddev = 2)')
+plt.title('Histogram of a Normal Distribution and a h(x) = x^3 function')
+plt.legend()
 
 plt.show()
