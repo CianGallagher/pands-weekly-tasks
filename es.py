1866d316bebaf67e534e6077ed415d876b81eb91
Cian Gallagher <ciangall1@gmail.com>
2024-04-25 11:59:51 +0100

    sys module imported to enable arguments from the command line being used in the script, script working

--- es.py
+++ es.py
@@ -1,11 +1,15 @@
 # Weekly task 07
 # Author Cian Gallagher
-# Program that reads a txt file in the same directory, counts the occurences of the letter e, and prints out that count.
+# Program that reads a txt file stated as an argument in command line, counts the occurences of the letter e, and prints out that count.
+# sys gives access to sys.argv which is a list of command line arguments passed to a python script. 
+import sys
 
-FILENAME = "moby-dick.txt"
+FILENAME = sys.argv[1]
+# In this case the FILENAME variable was assigned index 1 as the script itself is index 0. This enables the .txt file to be envoked from the command line in the following format - python es.py .\.moby-dick.txt
 with open(FILENAME, 'r') as f:
     str =  f.read()
-    print (str)
+
+    #print (str)
 
     count = str.lower().count("e")
 
