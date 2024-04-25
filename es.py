4c22f1ce8ab2db3807d1b11122162bd9a2758d5c
Cian Gallagher <ciangall1@gmail.com>
2024-04-25 10:56:23 +0100

    The count function was correct all along... human oversight

--- es.py
+++ es.py
@@ -1,13 +1,12 @@
 # Weekly task 07
 # Author Cian Gallagher
-# Program that reads a txt file in the same directory and prints a count of the letter e it contains.
-
-#The count method is not returning the expected number of es..
+# Program that reads a txt file in the same directory, counts the occurences of the letter e, and prints out that count.
 
 FILENAME = "moby-dick.txt"
-with open(FILENAME) as f:
+with open(FILENAME, 'r') as f:
     str =  f.read()
     print (str)
 
-    count = str.strip().lower().count("e")
-    print(count - 2)
+    count = str.lower().count("e")
+
+print('The letter e appears in this file',count, 'times.')
